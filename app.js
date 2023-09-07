45c87a8bbc722fde574adf60c9e7635fab0eba2f
millerronen <ronenmiller@gmail.com>
2023-09-08 01:06:11 +0300

    Don't reveal credentials

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const timerRoutes = require("./routes/timerRoutes");
--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config(); // Load variables from .env file
+
 module.exports = {
   // Server settings
   server: {
@@ -8,8 +11,8 @@ module.exports = {
   database: {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "ronenmiller@localhost",
-    password: process.env.DB_PASSWORD || "Miller2608!",
+    user: process.env.DB_USER || "your_db_user",
+    password: process.env.DB_PASSWORD || "your_db_password",
     databaseName: process.env.DB_NAME || "timersdb",
   },
 };
