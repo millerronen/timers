5cb8dcca87a0c67a2de9b5f20e050b40c8329cd4
millerronen <ronenmiller@gmail.com>
2023-09-09 22:11:00 +0300

    Update the README.md and change the webhook url

--- test/stress_test.sh
+++ test/stress_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Set the URL of your endpoint
-URL="http://localhost:3000/api/timers"  # Replace with your actual URL
+URL="http://localhost:3000/timers"  # Replace with your actual URL
 
 # Data for the POST request
 DATA='{
   "hours": 0,
   "minutes": 0,
-  "seconds": 10,
-  "url": "https://webhook.site/ace0963e-99c1-4be7-87b2-99afadb8cb2d"
+  "seconds": 5,
+  "url": "https://ronenmiller.free.beeceptor.com"
 }'  # Replace with your desired data
 
 # Loop to send 100 POST requests
-for i in {1..100}; do
+for i in {1..20}; do
   curl -X POST -H "Content-Type: application/json" -d "$DATA" "$URL" &
 done
 
